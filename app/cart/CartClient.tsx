8c569067d1a8a9505f3b815855cf4eb379ea7650
abdulrashid <abdulrashidkalodi@gmail.com>
2024-01-07 18:38:19 +0530

    cart product ui setted

---
+++ app/cart/CartClient.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { useCart } from "@/hooks/useCart";
+import Link from "next/link";
+import { MdArrowBack } from "react-icons/md";
+import Heading from "../components/Heading";
+import Button from "../components/Button";
+const CartClient = () => {
+  const { cartProducts } = useCart();
+  if (!cartProducts || cartProducts.length === 0) {
+    return (
+      <div className="flex flex-col items-center">
+        <div className="text-2xl ">Your Cart Is Empty</div>
+        <div>
+          <Link
+            href={"/"}
+            className=" text-slate-500 flex items-center gap-1 mt-2"
+          >
+            <MdArrowBack />
+            <span>Start Shoping</span>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <Heading title="Shopping Cart" center />
+      <div className="grid grid-cols-5 text-xs gap-4 pb-2 items-center mt-8">
+        <div className="col-span-2 justify-self-start">prduct</div>
+        <div className="justify-self-center">price</div>
+        <div className="justify-self-center">qty</div>
+        <div className="justify-self-end">totel</div>
+      </div>
+      <div>
+        {cartProducts &&
+          cartProducts.map((item) => {
+            return <div key={item.id}>{item.name}</div>;
+          })}
+      </div>
+      <div className="border-t-[1.5px] border-slate-200 py-4 flex justify-between gap-4">
+        <div className="w-[90px]">
+          <Button label="Clear cart" onClick={() => {}} small outline />
+        </div>
+        <div className="text-sm flex flex-col gap-1 items-start">
+          <div className="flex justify-between w-full text-base font-semibold">
+            <span>subtotel</span>
+            <span>$1000</span>
+          </div>
+          <p className="text-slate-500">
+            taxes&shipping calculation and check out
+          </p>
+          <Button label="Ckeck out" onClick={() => {}} />
+          <Link
+            href={"/"}
+            className=" text-slate-500 flex items-center gap-1 mt-2"
+          >
+            <MdArrowBack />
+            <span>Continue Shoping</span>
+          </Link>{" "}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartClient;
