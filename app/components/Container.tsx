3321e94b07d55ddd3ed5c6a38c63452be3253750
abdulrashid <abdulrashidkalodi@gmail.com>
2024-08-20 08:45:05 +0530

    an edits on container

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,9 +1,5 @@
 
-const Container = ({
-  children
-}: {
-  children: React.ReactNode
-}) => {
+const Container = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="max-w-[1920px] mx-auto xl:px-20 md:px-2 px-4">
       {children}
